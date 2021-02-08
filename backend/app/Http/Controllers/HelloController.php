737473986538847a9bca456b2ab6cde2bbd8b075
skonishi1125 <skonishi1125@gmail.com>
2021-02-08 17:11:42 +0900

    受け取った画像をpublic/imagesに保存する

--- backend/app/Http/Controllers/HelloController.php
+++ backend/app/Http/Controllers/HelloController.php
@@ -14,11 +14,18 @@ class HelloController extends Controller
     }
 
     public function view() {
+      // 任意の列でデータ検索する first(最初の1行だけ)
+      // 複数の条件で指定する場合は、getを使う 速習Laravelブクマ済
+      $php = Member::where('email','php@gmail.com')->first();
+
       $data = [
         'msg' => 'こんにちは、世界!',
         'number' => 100,
         'string' => 'うおお！',
         'members' => Member::all(),
+        // 主キーで検索する find()
+        'myadmin_email' => Member::find(1)->email,
+        'php' => $php,
       ];
       // hello/view.blade.phpを探しにいく
       return view('hello.view',$data);
@@ -26,24 +33,37 @@ class HelloController extends Controller
 
     // Requestクラスができていて、その中にform内容が入っている
     public function check(Request $req) {
+      $file = $req->picture;
+      $picName = $file->getClientOriginalName();
       $userData = [
         'name' => $req->name,
         'email' => $req->email,
         'password' => hash("sha256",$req->password),
+        'picture' => $picName,
       ];
 
+      // storage.app.filesに保存される
+      // $file->storeAs('files',$picName);
+      
+      // storage/appから、勝手に /public/images/画像が生成される
+      // $file->storeAs('./public/images',$picName);
+
+      // 正しくpublic.imagesに保存されている
+      $file->storeAs('images',$picName,'public_uploads');
+      
       return view('hello/check',$userData);
-
+      
     }
-
+    
     public function register(Request $req) {
       $userData = [
         'name' => $req->name,
         'email' => $req->email,
         // ハッシュ関数を取る
         'password' => $req->password,
+        'picture' => $req->picture,
       ];
-
+      
       return view('hello/register',$userData);
 
     }
